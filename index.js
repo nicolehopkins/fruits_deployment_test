23eadfa4282e33171dd04e2abcc45aa0a35e34da
Nicole L. Hopkins <nicolehopkins@pursuit.org>
2019-03-20 21:04:47 -0400

    changng local port

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.get('/ping', (req, res) => {
     res.json({success: pong});
 })
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 5011;
 app.listen(port, () => {
     console.log(`Listening on port ${port}`);
 })
